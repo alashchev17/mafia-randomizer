5d8e9062fef9d158392d7365ab3d035aae58ef7c
alashchev17 <andrew.lashchev15@gmail.com>
2023-11-13 03:10:29 +0100

    randomize of players' roles

--- src/utils/rolesRandomizer.ts
+++ src/utils/rolesRandomizer.ts
@@ -55,5 +55,19 @@ export const rolesRandomizer = (playersAmount: number) => {
     return arrayOfRoles;
   };
 
-  return generateArrayOfPlayers(mafiaPlayers, innocentPlayers);
+  const shuffleArray = (array: IPlayers[]) => {
+    const shuffledArray = array;
+
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = shuffledArray[i];
+      shuffledArray[i] = shuffledArray[j];
+      shuffledArray[j] = temp;
+    }
+    return shuffledArray;
+  };
+
+  const generatedArray = generateArrayOfPlayers(mafiaPlayers, innocentPlayers);
+
+  return shuffleArray(generatedArray);
 };
