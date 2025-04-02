200e4ee246746a3ca0c1420713fa26e44340afa8
alashchev17 <andrew.lashchev15@gmail.com>
2025-04-02 10:00:27 +0200

    feat: added custom Vercel deployment workflow

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 export default {
-  "*.{ts,tsx}": ["yarn format:write", "eslint --max-warnings 0", "git add"],
-  "*.{js,jsx}": ["yarn format:write", "git add"],
-  "*.{json,css,md}": ["yarn format:write", "git add"],
+  "*.{ts,tsx}": ["yarn format:write", "eslint --max-warnings 0"],
+  "*.{js,jsx}": ["yarn format:write"],
+  "*.{json,css,md}": ["yarn format:write"],
 };
